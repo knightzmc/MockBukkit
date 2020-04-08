701a5c416cab5a3942a3f8b6711293651a1b8d85
Alex Wood <alexljwood24@hotmail.co.uk>
2020-04-08 20:49:23 +0100

    Add Enchantment support!

---
+++ src/main/java/be/seeseemelk/mockbukkit/enchantment/BasicEnchantment.java
@@ -0,0 +1,66 @@
+package be.seeseemelk.mockbukkit.enchantment;
+
+import org.bukkit.NamespacedKey;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.enchantments.EnchantmentTarget;
+import org.bukkit.inventory.ItemStack;
+
+public class BasicEnchantment extends Enchantment
+{
+
+    private final String name;
+
+    public BasicEnchantment(String name)
+    {
+        super(NamespacedKey.minecraft(name));
+        this.name = name;
+    }
+
+    @Override
+    public String getName()
+    {
+        return name;
+    }
+
+    @Override
+    public int getMaxLevel()
+    {
+        return Integer.MAX_VALUE;
+    }
+
+    @Override
+    public int getStartLevel()
+    {
+        return 0;
+    }
+
+    @Override
+    public EnchantmentTarget getItemTarget()
+    {
+        return EnchantmentTarget.ALL;
+    }
+
+    @Override
+    public boolean isTreasure()
+    {
+        return false;
+    }
+
+    @Override
+    public boolean isCursed()
+    {
+        return false;
+    }
+
+    @Override
+    public boolean conflictsWith(Enchantment other)
+    {
+        return false;
+    }
+
+    @Override
+    public boolean canEnchantItem(ItemStack item)
+    {
+        return true;
+    }
+}
